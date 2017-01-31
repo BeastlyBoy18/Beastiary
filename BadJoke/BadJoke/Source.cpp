c1e3fb60d7cd04704ad02e530687b9ecf732196c
game110pm <game110pm@605-110-STU03>
2017-01-31 12:31:59 -0700

    Bad Joke

---
+++ BadJoke/BadJoke/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "What did the Big Chimney say to the little chimney" << endl << endl << endl << endl;
+
+
+	cout << "You're too young to be smoking" << endl;
+
+
+}
